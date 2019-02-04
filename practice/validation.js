3d1aa63808c19a1cca95521e102f89c420b4bb5b
KamalGaider <kamalgaider@gmail.com>
2019-02-04 21:40:57 +0100

    checkbox & dropdown

--- practice/validation.js
+++ practice/validation.js
@@ -5,18 +5,25 @@ appln.controller("cntrl", function($scope){
 	$scope.members = [
 	{
 		fname : 'will', 
-		lname : 'smith'
+		lname : 'smith',
+		genre : 'comedy',
+		retire : 'Not retired'	
 	},
 	{
 		fname : 'johnny', 
-		lname : 'depp'
+		lname : 'depp',
+		genre : 'action',
+		retire : 'Retired'
 	}
 	]
 
 	$scope.saveUser = function(user){
 		if($scope.userForm.$valid){
+			if(user.RETIRE == null){
+				user.RETIRE = "Not retired"
+			}
 			$scope.members.push({
-				fname : user.FNAME , lname : user.LNAME 
+				fname : user.FNAME , lname : user.LNAME , genre : user.GENRE , retire : user.RETIRE
 			});
 		}
 	}
